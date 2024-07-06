52b2ba4db8fa7de4d0b1bd7bb116e377844ba7e1
Lev-Shapiro <96536068+Lev-Shapiro@users.noreply.github.com>
2024-07-06 15:02:45 +0300

    remove more

--- src/ai-chat/ai-model.ts
+++ src/ai-chat/ai-model.ts
@@ -8,8 +8,6 @@ export enum AiModelName {
   gpt3turbo = 'gpt-3.5-turbo-0125',
   gpt4turbo = 'gpt-4-1106-preview',
   gpt4o = 'gpt-4o',
-  // Not to date, invalid exams:
-  mindopus = 'ft:gpt-3.5-turbo-1106:personal::9Pvy8ZEx',
 }
 
 export type ModelDataType = Record<AiModelName, AiModel>;
@@ -29,10 +27,5 @@ export const models: ModelDataType = {
     name: AiModelName.gpt4o,
     input: 0.005 / 1000,
     output: 0.015 / 1000,
-  },
-  [AiModelName.mindopus]: {
-    name: AiModelName.mindopus,
-    input: 0.003 / 1000,
-    output: 0.006 / 1000,
   },
 };
