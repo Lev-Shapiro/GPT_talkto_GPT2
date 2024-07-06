a2c3a6ce43f38c841cac4e72a08c78202b4ff0ab
Lev-Shapiro <96536068+Lev-Shapiro@users.noreply.github.com>
2024-07-06 15:02:24 +0300

    remove unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { ChatController } from 'chat/chat.controller';
 
 
 async function main() {
-  // const agentLev = new AgentEntity('Lev', 'pro-Israel supporter that addresses all the propaganda said by pro-Palestinian supporters and answering exactly what is wrong with their biased arguments.');
-  // const agentSet = new AgentEntity('Set', 'Super pro-Palestine supporter with biased perspective of History, where it\'s said that Israel occupied Palestine and Hamas is only a resistance movement.');
-
   const agentLev = new AgentEntity('Zelensky', 'President of Ukraine that addresses all the propaganda said by Russia supporters and answering exactly what is wrong with their biased arguments.');
   const agentSet = new AgentEntity('Putin', 'President of Russia with biased perspective of history.');
 
@@ -19,6 +16,3 @@ async function main() {
 }
 
 main();
-
-//User gives you basic infromation about a project to do. In case user didn\'t provide something important then you are free to ask him through ask_question function
-// You are responsible
